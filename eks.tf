d453d09511e7d6f8572b2448d1e89c737b42ea6a
Rishi Dadwal <rishidadwal62@gmail.com>
2024-01-27 22:49:18 +0530

    fix

--- eks.tf
+++ eks.tf
@@ -4,14 +4,14 @@ provider "aws" {
   region = "us-east-1"  # Update with your preferred AWS region
 }
 
-terraform {
-  required_providers {
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "~>1.14.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     kubectl = {
+#       source  = "gavinbunney/kubectl"
+#       version = "~>1.14.0"
+#     }
+#   }
+# }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
@@ -43,13 +43,13 @@ module "eks" {
     Terraform   = "true"
   }
 }
-provider "kubectl" {
-  config_path    = "~/.kube/config"  # Adjust the path as needed
-  context        = module.eks.cluster_id
-  cluster        = module.eks.cluster_endpoint
-  user           = "aws"
-  load_config_file = false
-}
+# provider "kubectl" {
+#   config_path    = "~/.kube/config"  # Adjust the path as needed
+#   context        = module.eks.cluster_id
+#   cluster        = module.eks.cluster_endpoint
+#   user           = "aws"
+#   load_config_file = false
+# }
 
 
 # Create an AWS Route 53 DNS zone for "arcstone.ai"
