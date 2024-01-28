7d9ebf464645ca993fd61f094bc516e5d0f87223
Rishi Dadwal <rishidadwal62@gmail.com>
2024-01-28 14:27:58 +0530

    aws feature

--- eks.tf
+++ eks.tf
@@ -71,3 +71,73 @@ resource "aws_route53_zone" "arcstone_zone" {
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = "portal26-${var.tenant_name}-unique"  
 }
+
+
+resource "aws_iam_role" "eks_cluster" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+  depends_on = [module.eks]
+}
+
+
+provider "aws" {
+  region = "us-west-2" # Change to your desired AWS region
+}
+
+
+
+resource "aws_iam_role_policy_attachment" "secrets_manager_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
+  role       = module.eks.worker_groups_role_name
+}
+
+resource "aws_db_instance" "rds_instance" {
+  identifier           = "portal26-${var.tenant_name}"
+  allocated_storage    = 20
+  engine               = "postgres"
+  engine_version       = "13.7"
+  instance_class       = "db.r5.large"
+  username             = "dbmaster"
+  password             = "ValidPassword123!"
+  availability_zone    = "us-east-1" # Replace with your desired availability zone
+  publicly_accessible  = false
+  final_snapshot_identifier = "final-snapshot-${var.tenant_name}"
+
+  multi_az              = false
+  storage_type          = "gp2"
+  backup_retention_period = 7
+
+  tags = {
+    Name = "portal26-${var.tenant_name}"
+    Environment = "dev"
+  }
+}
+
+resource "aws_secretsmanager_secret" "db_secret" {
+  name = "portal26.${var.tenant_name}.db"
+}
+
+resource "aws_secretsmanager_secret_version" "db_secret_version" {
+  secret_id = aws_secretsmanager_secret.db_secret.id
+  secret_string = <<EOT
+{
+  "url": "jdbc:postgresql://${aws_db_instance.rds_instance.endpoint}:5432/portal26-${var.tenant_name}",
+  "username": "dbmaster",
+  "password": "ValidPassword123!"
+}
+EOT
+}
